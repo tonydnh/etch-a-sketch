f2c6dda92c0dbd7d71330c65d677933d82b3f70e
Tony Dinh <tonydinh442@gmail.com>
2024-05-20 21:05:57 -0500

    Add better grid box system using flexbox

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".grid-container");
 
-for (let row = 1; row <= 16; row++) {
-    for (let col = 1; col <= 16; col++) {
-        const square = document.createElement("div");
-        square.className = "box";
-        square.id = `sq${row}-${col}`;
-        square.style.width = "50px";
-        square.style.height = "50px";
-        square.style.border = "1px solid black"
-        container.appendChild(square);
-    }
-}
+drawGrid(16);
 
-const boxes = document.querySelectorAll(".box");
+const boxes = document.querySelectorAll(".column");
 let held = false;
 
 for (const box of boxes) {
@@ -34,4 +24,17 @@ for (const box of boxes) {
 
 function color(element) {
     element.style.backgroundColor = "black";
+}
+
+function drawGrid(size) {
+    for (let r = 0; r < size; r++) {
+        const row = document.createElement("div");
+        row.className = "row"
+        for (let c = 0; c < size; c++) {
+            const column = document.createElement("div");
+            column.className = "column";
+            row.appendChild(column);
+        }
+        container.appendChild(row);
+    }
 }
